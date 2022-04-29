5a2641e668099e4a0dd33c3b99dfae63626908d0
juliancaceresf <julian.caceres@chilecompra.cl>
2022-04-29 13:18:09 -0500

    Implementacion de pruebas de plantas-lista

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'base-project'`, () => {
+  it(`should have as title 'Vivero El Otoño'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('base-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'base-project app is running!'
-    );
+    expect(app.title).toEqual('Vivero El Otoño');
   });
 });
--- src/app/plantas/plantas-lista/plantas-lista.component.spec.ts
+++ src/app/plantas/plantas-lista/plantas-lista.component.spec.ts
@@ -2,27 +2,67 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { PlantasListaComponent } from './plantas-lista.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantaService } from '../planta.service';
+import { Planta } from '../planta';
 
 describe('PlantasListaComponent', () => {
   let component: PlantasListaComponent;
   let fixture: ComponentFixture<PlantasListaComponent>;
+  let debug: DebugElement;
+
+  let createPlantas = (quantity: number): Planta[] => {
+    let storage = [];
+    for (let i = 0; i < quantity; i++) {
+      storage.push(
+        new Planta(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence()
+        )
+      );
+    }
+
+    return storage;
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantasListaComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule],
+      declarations: [PlantasListaComponent],
+      providers: [PlantaService],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantasListaComponent);
     component = fixture.componentInstance;
+
+    component.plantas = createPlantas(3);
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a table header', () => {
+    let listaElement: HTMLElement = fixture.nativeElement;
+    const encabezado = listaElement.querySelectorAll('thead tr')!;
+    expect(encabezado.length).toEqual(1);
+  });
+
+  it('should have three rows in its table body', () => {
+    let listaElement: HTMLElement = fixture.nativeElement;
+    const registros = listaElement.querySelectorAll('tbody tr')!;
+    expect(registros.length).toEqual(3);
   });
 });
